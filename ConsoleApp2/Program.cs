b9d4c16c892e2313972f9d8821494489ef899df2
3109917 <3109917@TE-GDA-D-D12C18>
2022-03-25 12:16:05 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Przesunięcie");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Hasło");
+char[] c = Console.ReadLine().ToCharArray();
+for (int i = 0; i < c.Length; i++)
+{
+    if (c[i] >= 'a' && c[i] <= 'z')
+    {
+        c[i] += (char)n;
+        if (c[i] > 'z') c[i] = (char)((c[i] - 'a') % ('z' - 'a') + 'a' - 1);
+    }
+    else if (c[i] >= 'A' && c[i] <= 'Z')
+    {
+        c[i] += (char)n;
+        if (c[i] > 'Z') c[i] = (char)((c[i] - 'A') % ('Z' - 'A') + 'A' - 1);
+    }
+    else if (c[i] >= '0' && c[i] <= '9')
+    {
+        c[i] += (char)n;
+        if (c[i] > '9') c[i] = (char)((c[i] - '0') % ('9' - '0') + '0' - 1);
+    }
+}
+Console.WriteLine(new string(c));
